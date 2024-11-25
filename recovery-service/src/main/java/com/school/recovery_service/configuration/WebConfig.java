5695fc1bb4e9aaa6148894e6aef56d1904e3ee37
Snezhana <snzhhnn@gmail.com>
2024-11-25 21:03:16 +0300

    change return type for date in all services

--- recovery-service/src/main/java/com/school/recovery_service/configuration/WebConfig.java
+++ recovery-service/src/main/java/com/school/recovery_service/configuration/WebConfig.java
@@ -1,9 +1,16 @@
 package com.school.recovery_service.configuration;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        WebMvcConfigurer.super.extendMessageConverters(converters);
+        converters.add(new MappingJackson2HttpMessageConverter(
+                new Jackson2ObjectMapperBuilder()
+                        .dateFormat(new StdDateFormat())
+                        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                        .build()));
     }
 }
