bdbd0e65c56180a0553a5428b1bc3ed380518fa6
Snezhana <snzhhnn@gmail.com>
2024-11-26 19:46:14 +0300

    теперь точно без ошибок

--- gateway-service/src/main/java/com/school/gateway_service/configuration/WebConfig.java
+++ gateway-service/src/main/java/com/school/gateway_service/configuration/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:5713")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
